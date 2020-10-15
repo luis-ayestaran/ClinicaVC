a70d1ffaa139462aa034b08972bd4f5c6036ea92
luis-ayestaran <46184203+luis-ayestaran@users.noreply.github.com>
2020-10-14 20:23:39 -0500

    Se limpian imports no usados en la clase MensajesSistema.java

--- jfxclinica/src/main/java/com/clinica/jfxclinica/configuration/MensajesSistema.java
+++ jfxclinica/src/main/java/com/clinica/jfxclinica/configuration/MensajesSistema.java
@@ -1,11 +1,8 @@
 package com.clinica.jfxclinica.configuration;
 
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
 import java.util.Properties;
 
 public class MensajesSistema {
