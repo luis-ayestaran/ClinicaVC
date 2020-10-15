0a2928b81a29e65c8d1f84bdf25ec05b89726501
luis-ayestaran <46184203+luis-ayestaran@users.noreply.github.com>
2020-10-14 20:19:27 -0500

    Primer Commit VC para app de Clinica - Agenda
    - Se agrega el formato de proyecto necesario para trabajar con interfaz
    grafica
    
    - Se crean las carpetas fxml, css, imagenes, tipografias en
    src/main/resources
    
    - Se crean paquetes para configuracion, vistas y controladores
    
    - Se agrega un archivo config.properties para mensajes de la aplicacion

---
+++ jfxclinica/src/main/java/com/clinica/jfxclinica/configuration/MensajesSistema.java
@@ -0,0 +1,36 @@
+package com.clinica.jfxclinica.configuration;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Properties;
+
+public class MensajesSistema {
+	//private static InputStream is;
+	private static Properties properties;
+		
+	static {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream is = classLoader.getResourceAsStream("config.properties");
+		properties = new Properties();
+		try {
+			properties.load(is);
+			is.close();
+		} catch (IOException e) {
+			e.printStackTrace(); 
+		}
+		
+	}
+	
+	public static String getMessage(String key) throws FileNotFoundException {
+		String message = null;
+		message = properties.getProperty(key);
+		if(message == null)
+			throw new FileNotFoundException("The property <"+ key +"> was not foud");
+		return properties.getProperty(key);
+	}
+	
+}
